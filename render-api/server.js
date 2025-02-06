0d5e530023d4a1ec52927ed3d213d450c9180bba
m0533199321 <m0533199321@gmail.com>
2025-02-06 23:43:49 +0200

    render-api2

--- render-api/server.js
+++ render-api/server.js
@@ -1,27 +1,6 @@
-const express = require('express');
-const axios = require('axios');
-require('dotenv').config();
+import renderApi from '@api/render-api';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Endpoint שמחזיר את רשימת האפליקציות
-app.get('/', async (req, res) => {
-    try {
-        const response = await axios.get('https://api.render.com/v1/services', {
-            headers: {
-                Authorization: `Bearer ${process.env.RENDER_API_KEY}`,
-                Accept: 'application/json'
-            }
-        });
-        res.json(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Something went wrong' });
-    }
-});
-
-// התחלת השרת
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+renderApi.auth('rnd_BowNn7C1SqvnVTaSzStIbTcC8qYW');
+renderApi.listServices({includePreviews: 'true', limit: '20'})
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
